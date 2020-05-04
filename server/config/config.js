7d88f0dfee7bb3f1c56d07232834c44ad7eb8617
Alberto Martínez <oyagum@gmail.com>
2020-05-04 18:08:26 +0200

    Codificamos la conexión a la base de datos

--- server/config/config.js
+++ server/config/config.js
@@ -15,6 +15,6 @@ let urlDB;
 if (process.env.NODE_ENV == 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://oyagum:Wrinkle2024!@cluster0-fzuhs.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.urlDB = urlDB;
